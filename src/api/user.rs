2374b50e7e9beaec74d191933d5e4daa66112d6e
CK Aznable <kami25565@gmail.com>
2023-03-18 07:26:17 +0000

    feat: impl User Tryfrom trait

--- src/api/user.rs
+++ src/api/user.rs
@@ -1,29 +1,32 @@
 use std::fmt::Display;
 
-pub enum UserKind {
-    Person,
+use scraper::{Selector, ElementRef};
+
+pub enum UserRace {
+    Human,
     Elf,
     Dwarf,
     Ork,
+    Unknown,
 }
 
-impl Display for UserKind {
+impl Display for UserRace {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            UserKind::Person => write!(f, "人類"),
-            UserKind::Elf => write!(f, "精靈"),
-            UserKind::Dwarf => write!(f, "矮人"),
-            UserKind::Ork => write!(f, "獸人"),
+            UserRace::Human => write!(f, "人類"),
+            UserRace::Elf => write!(f, "妖精"),
+            UserRace::Dwarf => write!(f, "矮人"),
+            UserRace::Ork => write!(f, "獸人"),
+            UserRace::Unknown => write!(f, "未知"),
         }
     }
 }
 
 pub struct User {
     id: String,
-    kind: UserKind,
+    race: UserRace,
     name: String,
     lv: u8,
-    title: String,
 }
 
 impl Default for User {
@@ -31,10 +34,81 @@ impl Default for User {
         let empty = "";
         User {
             id: empty.to_string(),
-            kind: UserKind::Dwarf,
+            race: UserRace::Dwarf,
             name: empty.to_string(),
             lv: 1,
-            title: empty.to_string(),
         }
+    }
+}
+
+impl User {
+    fn try_level_from_html(document: &ElementRef) -> Option<u8> {
+        let selector = Selector::parse(".userlevel").unwrap();
+        let lv = document
+            .select(&selector)
+            .next()?
+            .text()
+            .last()?
+            .to_string();
+
+        lv.trim().parse::<u8>().map_or(Some(0u8), |x|Some(x))
+    }
+
+    fn try_id_from_html(document: &ElementRef) -> Option<String> {
+        let selector = Selector::parse(".c-user__avatar").unwrap();
+        let id = document
+            .select(&selector)
+            .next()?
+            .value()
+            .attr("data-gamercard-userid")?
+            .to_string();
+
+        Some(id)
+    }
+
+    fn try_name_from_html(document: &ElementRef) -> Option<String> {
+        let selector = Selector::parse(".username").unwrap();
+        let name = document
+            .select(&selector)
+            .next()?
+            .text()
+            .next()?
+            .to_string();
+
+        Some(name)
+    }
+
+    fn try_race_from_html(document: &ElementRef) -> Option<UserRace> {
+        let selector = Selector::parse(".userrace").unwrap();
+        let race = document
+            .select(&selector)
+            .next()?
+            .value()
+            .attr("data-original-title")?;
+
+        let race = match race {
+            "人類" => UserRace::Human,
+            "矮人" => UserRace::Dwarf,
+            "妖精" => UserRace::Elf,
+            "獸人" => UserRace::Ork,
+            _ => UserRace::Unknown
+        };
+
+        Some(race)
+    }
+}
+
+impl TryFrom<&ElementRef<'_>> for User {
+    type Error = &'static str;
+
+    fn try_from(document: &ElementRef) -> Result<Self, Self::Error> {
+        let user = User {
+            lv: User::try_level_from_html(&document).ok_or("user lv invalid")?,
+            id: User::try_id_from_html(&document).ok_or("user id invalid")?,
+            name: User::try_name_from_html(&document).ok_or("user name invalid")?,
+            race: User::try_race_from_html(&document).map_or(UserRace::Unknown, |x|x),
+        };
+
+        Ok(user)
     }
 }
